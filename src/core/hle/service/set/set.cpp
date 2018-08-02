76c97e3274d42440dcaa7b3b8ade39f4fda398a4
yuzubot <yuzu@yuzu-emu.org>
2018-08-02 01:32:57 +0000

    Merge PR 851

--- src/core/hle/service/set/set.cpp
+++ src/core/hle/service/set/set.cpp
@@ -8,28 +8,9 @@
 #include "core/hle/kernel/client_port.h"
 #include "core/hle/kernel/client_session.h"
 #include "core/hle/service/set/set.h"
+#include "core/settings.h"
 
 namespace Service::Set {
-
-constexpr std::array<LanguageCode, 17> available_language_codes = {{
-    LanguageCode::JA,
-    LanguageCode::EN_US,
-    LanguageCode::FR,
-    LanguageCode::DE,
-    LanguageCode::IT,
-    LanguageCode::ES,
-    LanguageCode::ZH_CN,
-    LanguageCode::KO,
-    LanguageCode::NL,
-    LanguageCode::PT,
-    LanguageCode::RU,
-    LanguageCode::ZH_TW,
-    LanguageCode::EN_GB,
-    LanguageCode::FR_CA,
-    LanguageCode::ES_419,
-    LanguageCode::ZH_HANS,
-    LanguageCode::ZH_HANT,
-}};
 
 void SET::GetAvailableLanguageCodes(Kernel::HLERequestContext& ctx) {
     ctx.WriteBuffer(available_language_codes);
@@ -49,9 +30,17 @@ void SET::GetAvailableLanguageCodeCount(Kernel::HLERequestContext& ctx) {
     LOG_DEBUG(Service_SET, "called");
 }
 
+void SET::GetLanguageCode(Kernel::HLERequestContext& ctx) {
+    IPC::ResponseBuilder rb{ctx, 4};
+    rb.Push(RESULT_SUCCESS);
+    rb.Push(static_cast<u64>(available_language_codes[Settings::values.language_index]));
+
+    LOG_DEBUG(Service_SET, "called {}", Settings::values.language_index);
+}
+
 SET::SET() : ServiceFramework("set") {
     static const FunctionInfo functions[] = {
-        {0, nullptr, "GetLanguageCode"},
+        {0, &SET::GetLanguageCode, "GetLanguageCode"},
         {1, &SET::GetAvailableLanguageCodes, "GetAvailableLanguageCodes"},
         {2, nullptr, "MakeLanguageCode"},
         {3, &SET::GetAvailableLanguageCodeCount, "GetAvailableLanguageCodeCount"},
