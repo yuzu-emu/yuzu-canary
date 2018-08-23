026e794b3cbb75aa4e0f0c379e86ade673ed65ad
yuzubot <yuzu@yuzu-emu.org>
2018-08-23 22:31:49 +0000

    Merge PR 1163

--- src/audio_core/null_sink.h
+++ src/audio_core/null_sink.h
@@ -21,6 +21,10 @@ public:
 private:
     struct NullSinkStreamImpl final : SinkStream {
         void EnqueueSamples(u32 /*num_channels*/, const std::vector<s16>& /*samples*/) override {}
+
+        size_t SamplesInQueue() const override {
+            return 0;
+        }
     } null_sink_stream;
 };
 
