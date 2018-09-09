fd0866bb35e4a67bbd0cfc52ba3c052bd1f0e6db
yuzubot <yuzu@yuzu-emu.org>
2018-09-09 04:48:08 +0000

    Merge PR 1264

--- src/video_core/renderer_opengl/gl_rasterizer.cpp
+++ src/video_core/renderer_opengl/gl_rasterizer.cpp
@@ -525,8 +525,6 @@ void RasterizerOpenGL::DrawArrays() {
     state.Apply();
 }
 
-void RasterizerOpenGL::NotifyMaxwellRegisterChanged(u32 method) {}
-
 void RasterizerOpenGL::FlushAll() {}
 
 void RasterizerOpenGL::FlushRegion(VAddr addr, u64 size) {}
