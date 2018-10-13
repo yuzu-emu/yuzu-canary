d04600fd3694ceb94d24895462ef4b6d1d673bcf
yuzubot <yuzu@yuzu-emu.org>
2018-10-13 02:52:16 +0000

    Merge PR 1468

--- src/yuzu_cmd/config.cpp
+++ src/yuzu_cmd/config.cpp
@@ -125,10 +125,10 @@ void Config::ReadValues() {
 
     // System
     Settings::values.use_docked_mode = sdl2_config->GetBoolean("System", "use_docked_mode", false);
-    Settings::values.username = sdl2_config->Get("System", "username", "yuzu");
-    if (Settings::values.username.empty()) {
-        Settings::values.username = "yuzu";
-    }
+    const auto size = sdl2_config->GetInteger("System", "users_size", 0);
+
+    Settings::values.current_user =
+        std::clamp<int>(sdl2_config->GetInteger("System", "current_user", 0), 0, 7);
 
     // Miscellaneous
     Settings::values.log_filter = sdl2_config->Get("Miscellaneous", "log_filter", "*:Trace");
